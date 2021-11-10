5cd252d8a8083e29f90658ed417cf093fab32091
Marco Terzolo <marco.terzolo.8080@gmail.com>
2021-11-10 19:44:10 +0100

    fixed pg dependency iclashing with sequelizee merged more recent udagram

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -1,4 +1,3 @@
-import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import { sequelize } from "./sequelize";
@@ -8,32 +7,35 @@ import { IndexRouter } from "./controllers/v0/index.router";
 import bodyParser from "body-parser";
 import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
-(async () => { 
-
-  dotenv.config();
-
-  try {
-    console.log('sequelize', sequelize)
-    await sequelize.addModels(V0_FEED_MODELS);
-    console.log('1')
-    await sequelize.addModels(V0_USER_MODELS);
-    console.log('2')
-    await sequelize.sync();
-    console.log('3')
-    
-  } catch (error) {
-    console.log('error', error)
-  }
-
-
-  console.log("Database Connected");
+(async () => {
+  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);
+  console.log(
+    "process env start,",
+    process.env.POSTGRES_USERNAME,
+    "process end-----"
+  );
+  await sequelize.sync({force: true, logging: console.log});
 
   const app = express();
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
-  app.use(cors());
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      origin: "*",
+    })
+  );
 
   app.use("/api/v0/", IndexRouter);
 
